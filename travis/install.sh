13f7e57dd31699f600abab6ae32ddae93eb63c52
Safihre <safihre@sabnzbd.org>
2017-09-17 12:49:07 +0200

    Get Sleepless from PyPi during install

--- travis/install.sh
+++ travis/install.sh
@@ -12,9 +12,3 @@ python -c "import ssl; print ssl.OPENSSL_VERSION"
 # Python modules
 pip install --upgrade -r requirements.txt
 pip install --upgrade -r travis/requirements_osx.txt
-
-#################################
-# Sleepless module install
-cd builder/osx/sleepless/
-python setup.py install
-cd ../../../
